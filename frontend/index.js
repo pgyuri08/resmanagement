2e3ff5306359b0e0c5e49f3465c3c1cc8f651005
gyorgyperegi <gyorgy.peregi@trinitymirror.com>
2019-11-19 12:17:00 +0000

    code tidied up and tree structure added

--- frontend/index.js
+++ frontend/index.js
@@ -1,5 +1,3 @@
-let dataa;
-
   fetch('http://localhost:9000/job_descriptions')
   .then((res) => res.json())
   .then((data) => {
@@ -19,19 +17,10 @@ let dataa;
       fetch('http://localhost:9000/skills')
       .then((res) => res.json())
       .then((data) => {
-          // let output = '';
-          // data.forEach(function(post){
-          //     output+=`
-          //       <div>
-          //           <li>${post.name}(${post.description})</li>
-          //           <button name="skills" onclick="fetchRoles(${post._id})">Skills</button>
-          //       </div>  
-          //     `
-          // })
           let list = '';
           data[0].skills.forEach(function(skill){
             list +=`
-                  <ul id="ul">
+                  <ul id="skill-${skill._id}">
                     <li>Description: ${skill.description}</li>
                     <li>Name: ${skill.name}</li>
                     <li>Type: ${skill.type}</li>
@@ -41,77 +30,36 @@ let dataa;
           })
 
         let main = document.getElementById('output')
-        // document.getElementById('output').innerHTML = output
         let ul = document.createElement('ul')
+        ul.classList.add('skills-list')
         ul.innerHTML = list
         main.appendChild(ul)
           })
         }
 
 
-        function fetchQueries() {
+        function fetchQueries(param) {
           fetch('http://localhost:9000/skills/queries')
           .then((res) => res.json())
           .then((data) => {
-            // console.log(data[0].skills[0].queries[0])
-            //   let output = '';
-            //   data.forEach(function(post){
-            //       output+=`
-            //         <div>
-            //             <li>${post.name}(${post.description})</li>
-            //             <button name="queries" onclick="fetchRoles(${post._id})">Queries</button>
-            //         </div>  
-            //       `
-            //   })
-            //   let list = '';
-            //   data[0].skills.forEach(function(skill){
-            //     list +=`
-            //           <ul id="ul">
-            //             <li>Description: ${skill.description}</li>
-            //             <li>Name: ${skill.name}</li>
-            //             <li>Type: ${skill.type}</li>
-            //             <button name="queries" onclick="fetchQueries(${skill.queries}">Queries</button>
-            //           </ul>  
-            //     `
-            //   })
               let queries = '';
               data[0].skills.forEach(function(skill){
-                skill.queries.forEach(function(data){
-                  queries +=`
-                        <ul id="ul_queries">
-                          <li>Description: ${data._id}</li>
-                          <li>Name: ${data.query}</li>
-                          <li>Type: ${data.type}</li>
-                        </ul>  
-                  `
-
-                })
+                if(skill._id == param){
+                  skill.queries.forEach(function(data){
+                    queries +=`
+                          <ul id="ul_queries">
+                            <li>Description: ${data._id}</li>
+                            <li>Name: ${data.query}</li>
+                            <li>Type: ${data.type}</li>
+                          </ul>  
+                    `
+                  })
+                }
               })
     
-            let main = document.getElementById('output')
-            // document.getElementById('output').innerHTML = output
-            // let ul = document.createElement('ul')
-            // ul.innerHTML = list
-            // main.appendChild(ul)
-            let ul2 = document.createElement('ul_queries')
+            let main = document.querySelector('#skill-' + param)
+            let ul2 = document.createElement('ul')
             ul2.innerHTML = queries
             main.appendChild(ul2)
               })
             }
-
-      // function fetchRoles() {
-      //   return fetch('http://localhost:9000/roles/')
-      //   .then((res) => res.json())
-      //   .then((dataa) => {
-      //     createList();
-      //   })
-      //   .catch((err) => console.log(err))
-      // }
-
-      // function createList(dataa) {
-      //   let output = document.querySelector('#output')
-
-      //   let li_id = document.createElement('li');
-      //   li_id.innerText = dataa._id
-      //   output.appendChild(li_id);
-      // }
